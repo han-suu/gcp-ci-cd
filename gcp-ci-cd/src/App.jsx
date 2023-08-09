b3c2a9708812161553f63dcf4b692f2dc19228eb
root <root@PC-HAN>
2023-08-09 08:53:34 +0700

    Re3

--- gcp-ci-cd/src/App.jsx
+++ gcp-ci-cd/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Changesss + Content5</h1>
+      <h1>Original + Content6</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
